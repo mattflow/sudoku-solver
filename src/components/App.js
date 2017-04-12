ccbec12f794b13dbc6cadf5faf22f98af2df942d
Matt Flowers <mattmflowers28@gmail.com>
2017-04-12 13:46:49 -0400

    Add note about API

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ export default class App extends Component {
         <Container>
           <Header title="Sudoku Solver" />
           <SudokuBoard />
+          <p>Note: I am a broke college student, so I enroll in the free plans offered by most of the technology I use (mLab, Heroku and GitHub Pages). That being said, my API, which is hosted on Heroku, goes to sleep if it is not hit with requests for a short amount of time. If you press the 'Load Random' button and a puzzle does not load right away, give the API a couple seconds to wake up. I promise it will load!</p>
         </Container>
       </div>
     );
